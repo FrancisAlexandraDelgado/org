ba00edb4e6bf40b900f93aa7e6530a5bcfdaa914
FrancisAlexandraDelgado <francisdelgadoa@hotmail.com>
2023-08-05 18:01:18 -0600

    v 1.0

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,23 @@
+import "./ListaOpciones.css"
+   
+
+const ListaOpciones =(props)=>{
+
+    
+
+    const manejarCambio=(e)=>{
+        console.log("Cambio", e.target.value)
+        props.actualizarEquipo(e.target.value)
+
+    }
+    
+    return <div className="lista-opciones">
+        <label>Equipo</label>
+        <select value={props.valor} onChange={manejarCambio}> 
+            <option value="" disabled preventDefault hidden>Seleccionar equipo</option>
+            {props.equipos.map((equipo, index)=> <option key={index} value ={equipo}>{equipo}</option>)}
+        </select>
+    </div>
+}
+
+export default ListaOpciones
